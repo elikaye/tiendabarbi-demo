b43b1802eaf4ebca45b84f122651d518c0fc8b37
elikaye <elianakaye13@gmail.com>
2026-04-04 16:51:38 -0300

    Cambio de puerto 5000

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ IMPORTANTE: usar puerto de Railway
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // 🔗 Conexión DB y arranque
 (async () => {
